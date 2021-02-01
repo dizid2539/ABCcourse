e45745337ca7f5797c5ebe6be76968416ec93e96
임형근 <dizid2539@unist.ac.kr>
2021-02-01 18:06:33 +0900

    delete dog,cat function

--- git/animal.py
+++ git/animal.py
@@ -1,18 +1,10 @@
 import sys
-def cat():
-    print("meow")
-
-def dog():
-    print("woof")
 
 def default():
     print("hello")
 
 def main():
-    if sys.argv[1] == "dog":
-        dog()
-    else:
-        default()
+    default()
 
 if __name__ == "__main__":
     main()
