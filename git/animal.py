d6ebc51a7fa052f6a9760480addb34d580aa4ad5
임형근 <dizid2539@unist.ac.kr>
2021-02-01 18:14:31 +0900

    add cat function

--- git/animal.py
+++ git/animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def cat():
+    print("meow")
+
 def default():
     print("hello")
 
 def main():
-    default()
+    if sys.argv[1] == "cat":
+        cat()
+    else:
+        default()
 
 if __name__ == "__main__":
     main()
