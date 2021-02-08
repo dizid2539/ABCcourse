6c1078b5cc4f5f6390fdeb21b8ca7d6b3969c661
임형근 <dizid2539@numst.ac.kr>
2021-02-09 02:18:26 +0900

    fix some bugs (still doesn't work)

--- debugging/bubble.py
+++ debugging/bubble.py
@@ -1,7 +1,7 @@
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
-        for j in range(n):
+        for j in range(n-1):
             if arr[j] > arr[j+1]:
                 arr[j] = arr[j+1]
                 arr[j+1] = arr[j]
